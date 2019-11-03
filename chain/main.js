49619a70aeb815948ccd8f92b107bc71a9ef4fb9
Daniel Pyrathon <daniel@0xproject.com>
2019-11-02 23:07:44 -0700

    only use selected tokens

--- chain/main.js
+++ chain/main.js
@@ -5,16 +5,17 @@ let handler = require('./handler');
 let app = lotion({
 	initialState: {
 		fra: {
-			DAI: 100000,
-			USDC: 3000,
-			TERRA: 10000000,
+			DAI: 1000000000000000000000,
+			USDC: 100000000000000000000,
+			TERRA: 100000000000000000000,
 		},
 		pool: {
 			DAI: 100030,
 			USDC: 1000,
 			TERRA: 100010,
 		}
-	}
+	},
+	// genesisPath: '/Users/danielpyrathon/.lotion/networks/0b1053cc6f44648c67e072bbc1e81fac/config/genesis.json',
 });
 
 app.use(handler.handler);
